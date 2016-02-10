400756a93dc03476c64b6d0103cfefc6b9f6ba68
Derrick <derrick@BOXSERVER.telus>
2016-02-10 15:44:08 -0700

    Add sample visual studio project connecting to a database.

---
+++ VS2013-Database/WindowsFormsApplication1/WindowsFormsApplication1/DatabaseShiz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    class DatabaseShiz
+    {
+        private const String databaseName = "CMPT491-Warehouse"; 
+        private SqlConnection dbCon = null;
+
+
+        public DatabaseShiz(String server, String username) {
+            if (!connectDb(server, username)) throw new Exception("Failed to connect to db");
+        }
+
+
+        private bool connectDb(String server, String username) {
+            String conStr = "server="+ server +";" +
+                "Trusted_Connection=yes;" +
+                "User ID="+username+";" +
+                "database="+databaseName+";";
+
+            dbCon = new SqlConnection(conStr);
+            try
+            {
+
+                dbCon.Open();
+                //MessageBox.Show("db successfully connected");
+                return true;
+            }
+            catch (SqlException e)
+            {
+                //MessageBox.Show("failed to connect to db: " + e.Message);
+
+            }
+            return false; 
+        }
+    }
+
+
+}
---
+++ VS2013-Database/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        public void connectDB()
+        {
+            
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            DatabaseShiz theDB = null;
+
+            try
+            {
+                theDB = new DatabaseShiz("COMPSCI-PC", "compsci");
+                MessageBox.Show("Connected to database.");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                Application.Exit();
+            }
+
+            TreeNode parent = new TreeNode("top level");
+            TreeNode child = new TreeNode("öption 1");
+            parent.Nodes.Add(child);
+            parent.Nodes.Add(new TreeNode("öption 2"));
+            TreeNode something = new TreeNode("level2");
+            child.Nodes.Add(something);
+
+            treeView1.Nodes.Add(parent);
+            treeView1.Nodes.Add("something2");
+           
+        }
+
+    }
+}
